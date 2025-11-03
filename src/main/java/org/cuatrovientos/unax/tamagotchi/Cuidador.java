3325c2f91b50552a39bb8a76c74de5aac18e214e
UnaxGainzarain <unaxarbizu1@gmail.com>
2025-11-03 19:26:21 +0100

    Lógica de creación de hilos terminda

--- src/main/java/org/cuatrovientos/unax/tamagotchi/Cuidador.java
+++ src/main/java/org/cuatrovientos/unax/tamagotchi/Cuidador.java
@@ -1,10 +1,38 @@
 package org.cuatrovientos.unax.tamagotchi;
 
-public class Cuidador {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
+public class Cuidador {
+	private static List<Tamagotchi> tamagotchiList = new ArrayList<>();
+	private static List<Thread> threadList = new ArrayList<>();
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		Random rand = new Random();
+		String leer;
+		
+		System.out.println("Cuantos tamagotchis quieres cuidar??");
+		leer = sc.nextLine();
+		int numTamagotchis = Integer.parseInt(leer);
+		
+		// Ahora lanzamos los tamagotchis al mundo (poner pilas y lanzar)
+		// Segun cuantos tagotchis pida el usuario lanzamos uno por cada tamagotchi
+		for (int i =0 ; i < numTamagotchis; i++) {
+			int eatSpeed = rand.nextInt(5001)+ 3000;
+			
+			Tamagotchi t = new Tamagotchi("Tama-" + i, eatSpeed);
+			tamagotchiList.add(t);
+			Thread tThread = new Thread(t);
+			threadList.add(tThread);
+			tThread.start();
+		}
+		
+		// Bucle principal del cuidadro 
+		
+		
 	}
 
 }
